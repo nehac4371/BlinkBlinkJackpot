fae9a321c7245918a1216df8d2fd6bd9722547c7
kanchanSarolkar <31560198+kanchanSarolkar@users.noreply.github.com>
2020-02-29 12:20:17 +0530

    Add files via upload

--- slots.py
+++ slots.py
@@ -46,9 +46,9 @@ class MyImageViewerWidget(QFrame):
             if q.empty() == False:
                 q_cnt = q.get() 
             
-            #print('slots q_cnt:',q_cnt)
+        
             if q_cnt < 1:
-                #print('Blink 1:',q_cnt)
+                
 
                 a = random.randint(0, len(self.x) - 1)
                 self.rect = QRect(self.x[a], self.y[a], 300, 300)
@@ -66,7 +66,7 @@ class MyImageViewerWidget(QFrame):
                 self.ui.mLabel3.setPixmap(cropped)
 
             elif q_cnt < 2:
-                #print('Blink 2:',q_cnt)
+                
                 b = random.randint(0, len(self.x) - 1)
                 self.rect = QRect(self.x[b], self.y[b], 300, 300)
                 cropped = self.px.copy(self.rect)
@@ -78,7 +78,6 @@ class MyImageViewerWidget(QFrame):
                 self.ui.mLabel3.setPixmap(cropped)
 
             elif q_cnt < 3:
-                #print('Blink 3:',q_cnt)
 
                 c = random.randint(0, len(self.x) - 1)
                 self.rect = QRect(self.x[c], self.y[c], 300, 300)
@@ -109,8 +108,6 @@ class MyMainWindow(QMainWindow):
     def __init__(self , parent=None):
         
         QWidget.__init__(self, parent=parent)
-        # attributs de la fenetre principale
-        #self.q = queue.Queue()
         self.setGeometry(500, 450, 940, 320)
         self.setFixedSize(940, 320)
         self.setWindowTitle('Slot Machine')
@@ -120,7 +117,6 @@ class MyMainWindow(QMainWindow):
     
 
     def keyPressEvent(self, e):
-        #print('inside KeyPress')
         if e.key() == QtCore.Qt.Key_Space:
             self.mDisplay.spin()
 
@@ -129,7 +125,6 @@ class MyMainWindow(QMainWindow):
 def QTWin():
     app = QApplication(sys.argv)
     w = MyMainWindow()
-    #w.get_que(q)
     w.show()
     app.exec_()
 
